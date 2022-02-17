a7752d589197055d6d21d26b4784a3af7ec98597
Darudul <darudulislam@gmail.com>
2022-02-17 20:38:57 +0800

    add error

--- money master/js/script.js
+++ money master/js/script.js
@@ -1,12 +1,15 @@
-// function getInputValue(){
-//   const foodInput = document.getElementById("food-input");
+// function getInputValue(inputId) {
+//   const foodInput = document.getElementById(inputId);
 //   const foodInputText = parseFloat(foodInput.value);
+//   foodInput.value = "";
+
+//   return foodInputText;
 // }
-
-
 
 document.getElementById("calculate-btn").addEventListener("click", function () {
   // console.log("test");
+  const income = document.getElementById("income");
+  const incomeAmount = parseFloat(income.value);
   const foodInput = document.getElementById("food-input");
   const foodInputText = parseFloat(foodInput.value);
   // console.log(foodInputText);
@@ -17,6 +20,20 @@ document.getElementById("calculate-btn").addEventListener("click", function () {
   const clothesInputText = parseFloat(clothesInput.value);
   // console.log(clothesInputText);
 
+  if (
+    foodInput.value < 0 ||
+    foodInput.value == "" ||
+    rentInput.value < 0 ||
+    rentInput.value == "" ||
+    clothesInput.value == "" ||
+    clothesInput.value < 0 ||
+    income.value < 0 ||
+    income.value == ""
+  ) {
+    alert("please enter positive number");
+    return;
+  }
+
   const totalExpenses = document.getElementById("total-expenses");
   const totalExpensesText = parseFloat(totalExpenses.innerText);
   // console.log(totalExpensesText);
@@ -25,24 +42,31 @@ document.getElementById("calculate-btn").addEventListener("click", function () {
   const totalExpencesAmount = previousTotalExpencesAmount + totalExpensesText;
   totalExpenses.innerText = totalExpencesAmount;
 
-  const income = document.getElementById("income");
-  const incomeAmount = parseFloat(income.value);
   const balance = document.getElementById("balance");
   const balanceText = parseFloat(balance.innerText);
   const balanceAmount = incomeAmount - totalExpencesAmount;
   const balanceTotalAmount = balanceText + balanceAmount;
   balance.innerText = balanceTotalAmount;
-  foodInput.value = "";
-  rentInput.value = "";
-  clothesInput.value = "";
+  // foodInput.value = "";
 });
 
+// savings part start
 document.getElementById("save-btn").addEventListener("click", function () {
   const income = document.getElementById("income");
   const incomeAmount = parseFloat(income.value);
 
   const saveInput = document.getElementById("save-input");
   const saveAmount = parseFloat(saveInput.value);
+
+  if (
+    income.value < 0 ||
+    income.value == "" ||
+    saveInput.value < 0 ||
+    saveInput.value == ""
+  ) {
+    alert("please enter valid number");
+    return;
+  }
 
   const savingAmount = document.getElementById("saving-amount");
   const savingAmountText = parseFloat(savingAmount.innerText);
@@ -60,6 +84,4 @@ document.getElementById("save-btn").addEventListener("click", function () {
 
   const remainingBalanceTotal = remaingBalance + remaingBalanceAmount;
   remaingBal.innerText = remainingBalanceTotal;
-
-  saveInput.value = "";
 });
